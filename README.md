8c3b32613f1c2acd1c2548a73bddef17e6ce915f
godha <me@godha.cn>
2019-12-31 17:16:42 +0800

    new commit

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 MousePlayer is a web player , it develop with Javascript and be used for pc browser;
 
 If you want to be use for pe browser,you can see this project : MousePlayer-pe
+
+__________________
+
+
+# Start
+
+At frist ,Introducing file of mouseplayer in your html, and create a div
+
+You can choose two load modes
+
+##### First kind : only a single JS file is introduced , CSS integration to JS file 
+
+```html
+<div id="player"></div>
+<script src="MousePlayer.agg.min.js"></script>
+```
+
+##### Second kind :  introduction of CSS and JS files at the same time , this will help to modify the style
+
+```html
+<link rel="stylesheet" href="MousePlayer.min.css" />
+<div id="player"></div>
+<script src="MousePlayer.min.js"></script>
+```
+
+
+
+
--- ShowHow/mouseplayer.js
+++ ShowHow/mouseplayer.js
@@ -1,41 +1,65 @@
 /*!
  * MousePlayer v1.0.0
- * 
+ * Author is Gott.ha / Godha
+ * Official website: mouseplayer.godha.cn
+ * Github repository: https://github.com/godaha/MousePlayer (English)
+ * Gitee repository: https://gitee.com/godha/mouseplayer (简体中文).
  */
 
 function MousePlayer(parameter){
-	let infor = [];
+
+	function isUndef(d){
+		return d === undefined || d === null || d === ''
+	}
+
+	function isTrue(d){
+		return d === true
+	}
+
+	function isFalse(d){
+		return d === false
+	}
+
+
+
+	/**
+    * Basic parameter inspection.
+    * If the parameter is wrong, this ends all and prompts for an error.
+    */
+
+	var infor = [];
 	infor.error_att = {
 		nodom:"Container does not exist \n\nPlease check the < container > attribute",
 		nexterr:"'next' attribute must be a function",
 		enderr:"'end' attribute must be a function"
-	}//属性错误
+	}
+
 	function error_att(type){
 		console.error("Attribute error: "+infor.error_att[type]);
-	}//属性错误提示
-
-
-	////////检查属性
-
+	}
 	function typefunc(dom){
-		let typs = typeof(parameter[dom]);
-		if(typs!='function'&&typs!='undefined'){return 1;}else{return 0;}
-	}
-	if(typeof(parameter.container)!='object'){error_att('nodom');return;}
-	if(typefunc('next')){error_att('nexterr');return;}
-	if(typefunc('end')){error_att('enderr');return;}
-	if(){
-		
+		var typs = typeof parameter[dom];
+		return typs!='function'&&typs!='undefined'
 	}
 
-	////////
+	
 
 
+	if(typeof parameter.container!='object'){
+		error_att('nodom');
+		return
+	}
+	if(typefunc('next')){
+		error_att('nexterr');
+		return
+	}
+	if(typefunc('end')){
+		error_att('enderr');
+		return
+	}
 
 
-
-	//上面为基础检查
-	/****************************************/
+	/*  */
 
 
 	
